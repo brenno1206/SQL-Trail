e788c3e3a79652ceae46b271be58d6cd1a529e3d
Brenno Gomes <brennogbreda@gmail.com>
2026-04-09 19:15:57 -0300

    Adicionando icons

--- frontend/assets/icons.ts
+++ frontend/assets/icons.ts
@@ -1,9 +1,38 @@
 export {
-  IoIosAirplane, //  Companhias Aérea
   IoMdPeople, //  Recursos Humanos
   IoMdSchool, //  Universidade
-  IoIosCart, // E-commerce
   IoIosArrowDropleftCircle, // Voltar
+  IoIosBuild, // Configurações
+  IoIosLogIn, // Login
+  IoIosMore, // Mais opções
+  IoIosRefresh, // Atualizar
+  IoIosStarOutline, // Especial
+  IoIosWarning, // Aviso
+  IoMdAttach, // Anexo
+  IoMdCheckmarkCircleOutline, // Sucesso
+  IoMdCloseCircleOutline, // Erro
+  IoMdFunnel, // Filtro
+  IoMdSearch, // Pesquisa
+  IoMdStats, // Estatísticas
+  IoMdStopwatch, // Tempo
 } from 'react-icons/io';
 
+export {
+  LuTestTubeDiagonal, // Teste
+} from 'react-icons/lu';
+
 // https://react-icons.github.io/react-icons/icons/io/
+
+export {
+  FaMap, // Mapa
+  FaDatabase, // Banco de dados
+} from 'react-icons/fa';
+
+export {
+  TiThMenu, // Menu
+} from 'react-icons/ti';
+
+export {
+  MdLogout, // Logout
+  MdLogin, // Login
+} from 'react-icons/md';
